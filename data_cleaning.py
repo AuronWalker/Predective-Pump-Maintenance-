1458d7ffcf380cd889266cee8ec7bad54b71a39a
Auron <52473536+WigglyGull@users.noreply.github.com>
2025-10-08 17:22:57 +1300

    final model design

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import kurtosis, skew
 
 def convert_ac_to_dc(input_voltage_csv, input_current_csv, input_vibration_csv, output_csv):
     df_voltage = pd.read_csv(input_voltage_csv)
@@ -19,15 +20,43 @@ def convert_ac_to_dc(input_voltage_csv, input_current_csv, input_vibration_csv, 
     current_rms = np.sqrt(np.mean(current_ac**2, axis=1))
     vibration = np.mean(vibrations, axis=1)
 
+
+    voltage_series = pd.Series(voltage_rms)
+    current_series = pd.Series(current_rms)
+    vibration_series = pd.Series(vibration)
+
+    window = 1000
+    voltage_mean = voltage_series.rolling(window).mean()
+    voltage_var = voltage_series.rolling(window).var()
+    # df['voltage_rms_skew'] = df['voltage_rms'].rolling(window).apply(lambda x: skew(x, bias=False))
+    # df['voltage_rms_kurt'] = df['voltage_rms'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
+
+    current_mean = current_series.rolling(window).mean()
+    current_var = current_series.rolling(window).var()
+    # df['current_rms_skew'] = df['current_rms'].rolling(window).apply(lambda x: skew(x, bias=False))
+    # df['current_rms_kurt'] = df['current_rms'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
+
+    vibration_mean = vibration_series.rolling(window).mean()
+    vibration_var = vibration_series.rolling(window).var()
+    # df['vibration_skew'] = df['vibration'].rolling(window).apply(lambda x: skew(x, bias=False))
+    # df['vibration_kurt'] = df['vibration'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
+
     df_dc = pd.DataFrame({
         "time": time,
         "voltage_rms": voltage_rms,
         "current_rms": current_rms,
-        "vibration": vibration  
+        "vibration": vibration,
+        "voltage_mean" :  voltage_mean,
+        "current_mean": current_mean,
+        "vibration_mean": vibration_mean,
+        "voltage_var": voltage_var,
+        "current_var": current_var,
+        "vibration_var": vibration_var
     })
 
+    df_dc = df_dc.iloc[window:] #removes rows without extra stats
     df_dc.to_csv(output_csv, index=False)
-    # print(f"DC-equivalent single channel file saved as '{df_dc}'")
+    print(f"DC-equivalent single channel file saved as '{df_dc}'")
 
 ac_files = ['impeller', 'healthy', 'unbalanced_motor', 'unbalanced_pump', 'cavitation_discharge', 'cavitation_suction']
 
--- random_forest.py
+++ random_forest.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import kurtosis, skew
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
@@ -13,31 +12,10 @@ def create_training_data():
 
     for i in range(len(data_files)):
         df = pd.read_csv('./Cleaned Data\{}.csv'.format(data_files[i]))
-
-        # Add this to data_cleaning.py at home
-        # Adds uneeded time
-        window = 1000
-        df['voltage_rms_mean'] = df['voltage_rms'].rolling(window).mean()
-        df['voltage_rms_var'] = df['voltage_rms'].rolling(window).var()
-        df['voltage_rms_skew'] = df['voltage_rms'].rolling(window).apply(lambda x: skew(x, bias=False))
-        df['voltage_rms_kurt'] = df['voltage_rms'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
-
-        df['current_rms_mean'] = df['current_rms'].rolling(window).mean()
-        df['current_rms_var'] = df['current_rms'].rolling(window).var()
-        df['current_rms_skew'] = df['current_rms'].rolling(window).apply(lambda x: skew(x, bias=False))
-        df['current_rms_kurt'] = df['current_rms'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
-
-        df['vibration_mean'] = df['vibration'].rolling(window).mean()
-        df['vibration_var'] = df['vibration'].rolling(window).var()
-        df['vibration_skew'] = df['vibration'].rolling(window).apply(lambda x: skew(x, bias=False))
-        df['vibration_kurt'] = df['vibration'].rolling(window).apply(lambda x: kurtosis(x, bias=False))
-
         df['fault'] = data_files[i]
 
-        #This could be useful later 
+        #This could be useful later
         df = df.drop(columns = ['time'])
-        df = df.drop(columns = ['voltage_rms'])
-        df = df.drop(columns = ['current_rms'])
 
         df_list.append(df)
     
@@ -47,24 +25,36 @@ def create_training_data():
 def create_random_forest(training_data):
     X = training_data.iloc[:, :-1]
     y = training_data.iloc[:, -1]
-    print(X.head())
-    print(y.head())
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=69, test_size=0.2)
 
-    # rf = RandomForestClassifier(n_estimators = 1000,
-    #                             criterion = 'entropy',
-    #                             min_samples_split = 10,
-    #                             max_depth = 14,
-    #                             random_state = 42)
-    rf = RandomForestClassifier()
+    rf = RandomForestClassifier(n_estimators = 30,
+                                criterion = 'gini',
+                                min_samples_split = 5,
+                                max_depth = 8,
+                                random_state = 42)
+    
+    #No hyper parameters
+    # rf = RandomForestClassifier()
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
 
     #shows what columns had the biggest impact
     features = pd.DataFrame(rf.feature_importances_, index=X.columns)
-    print(features.head())
+    print(features.head(9))
 
     print(classification_report(y_test, y_pred))
+
+    n_nodes = [estimator.tree_.node_count for estimator in rf.estimators_]
+    max_depths = [estimator.tree_.max_depth for estimator in rf.estimators_]
+
+    print(f"Number of trees: {len(rf.estimators_)}")
+    print(f"Average nodes per tree: {np.mean(n_nodes):.1f}")
+    print(f"Average depth per tree: {np.mean(max_depths):.1f}")
+    print(f"Total nodes: {np.sum(n_nodes)}")
+
+    total_nodes = np.sum([est.tree_.node_count for est in rf.estimators_])
+    model_size_bytes = total_nodes * 28
+    print(f"Estimated model size: {model_size_bytes/1024:.2f} KB")
 
 
 training_data = create_training_data()
